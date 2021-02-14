44f2a99e0ab7adeec778952ed2031637af0d6897
Prince Kaizen Namwali <33nano@pm.me>
2021-02-14 05:49:17 +0000

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flame/flame.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:spritewidget/spritewidget.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
+import 'package:flame_splash_screen/flame_splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// nothing yet...
